5737911d8b9036712f0f01dc4fd00b53e2ae64c4
Arshia-Rastgoo <arshiarastgoo4@gmail.com>
2025-09-18 17:16:04 +0330

    stage one

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import Fottercomponent from "./Components/UI/Fotter.component";
 import Navbarcomponent from "./Components/UI/Navbar.component";
-
+import { Routes,Route } from "react-router-dom";
+import Homepage from "./Pages/Home.page";
+import FAQpage from "./Pages/FAQ.page";
+import AboutUspage from "./Pages/AboutUs.page";
+import Contactuspage from "./Pages/Contactus.page";
 function App() {
   return (
     <div >
@@ -8,7 +12,12 @@ function App() {
       <Navbarcomponent/>
      
       <div className="wrapper">
-        wrapper
+       <Routes>
+        <Route path="/Home" element={Homepage}/>
+        <Route path="/FAQ" element={FAQpage}/>
+        <Route path="/AboutUs" element={AboutUspage}/>
+        <Route path="/Contact-us" element={Contactuspage}/>
+       </Routes>
       </div>
       fotter
       <Fottercomponent/>
---
+++ src/Pages/FAQ.page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function FAQpage() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
