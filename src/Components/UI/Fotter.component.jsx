3b4748f08c8fea44414633451382ccb29077b91a
Arshia-Rastgoo <arshiarastgoo4@gmail.com>
2025-09-19 13:19:39 +0330

    Fotter added

--- src/Components/UI/Fotter.component.jsx
+++ src/Components/UI/Fotter.component.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-
+import "./Fotter.css"
 export default function Fottercomponent() {
   return (
-    <div>
-      
-    </div>
+    <footer className='footer'>
+      <span> Arshia‌‌ ‌s' Crypto Tracking Blog</span>
+      <span className='d-none d-md-inline'>.  All rights reserved. </span>
+    </footer>
   )
 }
--- src/Components/UI/Navbar.component.jsx
+++ src/Components/UI/Navbar.component.jsx
@@ -1,33 +1,40 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React from 'react'
+import React, { useRef } from 'react'
 import { Link } from 'react-router-dom'
 import { FaCoins } from 'react-icons/fa'
 import './Navbar.css'
+
 export default function Navbarcomponent() {
+
+  const btnToggleRef = useRef();
+  
+  const toggleMenu=()=>{
+    btnToggleRef.current.click()
+  }
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
       <div className="container-fluid">
-        <div> 
+        <div>
           <span className='navbar-text'><FaCoins className='navbar-logo' /></span>
           <span className='name1'>   Crypto Tracking </span>
-    
+
         </div>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <button className="navbar-toggler" ref={btnToggleRef} type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item">
-              <Link className="nav-link" to="/Home">Home</Link>
+              <Link className="nav-link" onClick={toggleMenu} to="/Home">Home</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/FAQ">FAQ</Link>
+              <Link className="nav-link" onClick={toggleMenu} to="/FAQ">FAQ</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/AboutUs">AboutUs</Link>
+              <Link className="nav-link" onClick={toggleMenu} to="/AboutUs">AboutUs</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/Contact-us">Contact-us</Link>
+              <Link className="nav-link"  onClick={toggleMenu} to="/Contact-us">Contact-us</Link>
             </li>
           </ul>
         </div>
