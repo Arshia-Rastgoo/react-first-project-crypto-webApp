129fe3ec826a5541f16c6fae05bb22a6b56a5672
Arshia-Rastgoo <arshiarastgoo4@gmail.com>
2025-09-20 20:12:55 +0330

    API

--- src/Pages/Home.page.jsx
+++ src/Pages/Home.page.jsx
@@ -1,11 +1,40 @@
-import React from 'react'
+import React, { use, useEffect, useState } from 'react'
 import Spinnercomponent from '../Components/UI/Spinner.component'
-
+import axios from 'axios';
+import { baseUrl } from '../constants/api';
 export default function Homepage() {
-  return (
-    <div>
-      <Spinnercomponent />
 
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState([false]);
+  const [error, setError] = useState([""]);
+
+  useEffect(() => {
+    setLoading(true);
+    axios(baseUrl)
+      .then(response => {
+        setLoading(false);
+        console.log(response.data);
+
+      })
+      .catch(error => setError("An Error has occured !"))
+  }, []);
+
+  return (
+
+    <div className='container'>
+      <div className='row'></div>
+      <div className='col-12 text-center' >
+        <h1>Crypto Tracking</h1>
+        <h4>A place to manage all your trade informations and actions</h4>
+        <h4>Do not forget to use risk management for your assets !!</h4>
+      </div>
+
+      <div className='row'>
+        <div className='col-12' >
+          {loading ? <Spinnercomponent /> : error ==="" ? <h1>{error}</h1> : <h1>Data ready</h1>}
+          
+        </div>
+      </div>
     </div>
   )
 }
